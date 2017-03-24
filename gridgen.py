12882cb84669e7f34315e504b11c040d75264aed
rvollmer <rvollmer@aypac.de>
2017-03-24 17:35:12 +0100

    Grid workz now

--- gridgen.py
+++ gridgen.py
@@ -18,31 +18,66 @@ import numpy as np
 #import scipy
 from scipy.stats import maxwell
 #import xlwt
+import matplotlib.pyplot as plt
 
 
-totalNumberOfParticles=40
-x,y=2,2
-boxes = [[[]]*x]*y
-density = np.random.rand(x,y)
+def f(pos):
+    return ((pos[0]-5)**2 + (pos[1]-5)**2)
+    
+def integral():
+    f(boxes)
+
+totalNumberOfParticles = 1000
+numberOfBoxesPerDimension = 5
+boxes = np.array([[0]*numberOfBoxesPerDimension]*numberOfBoxesPerDimension, dtype=np.ndarray)
+volumes = np.array([[0]*numberOfBoxesPerDimension]*numberOfBoxesPerDimension, dtype=float)
+density = np.ones([numberOfBoxesPerDimension,numberOfBoxesPerDimension])#np.random.rand(numberOfBoxesPerDimension, numberOfBoxesPerDimension)#np.ones([x,y])#
 density /= np.sum(density)
-boxSize = 5
+numberOfParticles = density*totalNumberOfParticles
+numberOfParticles = numberOfParticles.astype(int)
+
+boxSize = 1/numberOfBoxesPerDimension
 dim=2
 
 for i in range(len(boxes)):
-    for j in range(len(boxes[:])):
-        print("generate box "+str(i)+":"+str(j))
-        particlesInBox = int(density[i,j]*totalNumberOfParticles)
+    for j in range(len(boxes[:])):        
+        particlesInBox = round(density[i,j]*totalNumberOfParticles)
         particlesPerDirection = int(particlesInBox**(1/dim))
         directParticlesInBox = particlesPerDirection**dim
+
+        box = [(i*boxSize), (j*boxSize)]+np.random.rand((particlesInBox-directParticlesInBox), dim)*boxSize        
         
-        boxes[i][j] = [[0]*dim]*directParticlesInBox
-        a=np.linspace((i*boxSize), ((i+1)*boxSize), particlesPerDirection)
-        print(a)
-        b=np.linspace((j*boxSize), ((j+1)*boxSize), particlesPerDirection)
-        print(b)
-        box = np.transpose([np.tile(a, len(b)), np.repeat(b, len(a))])
-        print(box)
-        boxes[i][j] = box
+        if particlesPerDirection > 0:
+            l = boxSize/particlesPerDirection
+            
+            #boxes[i][j] = [[0]*dim]*directParticlesInBox
+            a=np.linspace((i*boxSize), ((i+1)*boxSize), particlesPerDirection, endpoint=False)
+            b=np.linspace((j*boxSize), ((j+1)*boxSize), particlesPerDirection, endpoint=False)
+            box1 = np.transpose([np.tile(a, len(b)), np.repeat(b, len(a))])
+            box1 += np.random.rand(directParticlesInBox, dim)*l
+            box = np.concatenate([box1, box])
+        boxes[i,j] = box
+        volumes[i,j] = boxSize**dim/particlesInBox
+        
+boxes = np.array(boxes)
+arr=np.concatenate(np.concatenate(boxes, axis=0), axis=0)
+
+plt.rcParams["figure.figsize"] = [8,8]
+plt.figure()
+        
+ax = plt.gca()
+ax.set_xlim([0,1])
+ax.set_ylim([0,1])
+
+
+#plt.plot(arr, 'ro')
+plt.plot(arr[:,0],arr[:,1], 'r.')
+
+
+plt.draw()
+#plt.savefig(SAVE_PATH+"/"+str(simNum)+"-"+key+".png", box_inches='tight', dpi=100)
+plt.show()
+plt.close()
         
         
 
